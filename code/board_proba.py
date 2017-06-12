a03cff34f92865b307cf0f5db62f370da4b70e71
pvp23 <patrick.v.platen@gmail.com>
2017-06-12 13:36:53 +0100

    added code for action and community cards + added code for stationnary vector

--- code/board_proba.py
+++ code/board_proba.py
@@ -5,9 +5,10 @@ Created on Tue May  16 2017
 @author: john
 """
 import numpy as np
+from discreteMarkovChain import markovChain
 np.set_printoptions(threshold=np.nan) # this line allows to print all elements of matrix
 
-def fillMatrix():
+def fillMatrixWithDiceProb():
     """
     Fill the two board transition matrices following their get out of jail strategy:
     - M_DOUBLE : wait 3 times and try to make double strategy,
@@ -36,7 +37,40 @@ def fillMatrix():
         else:
             M_fine[i,i-n+2:l+(i-n+2)] = dice_prob 
     return M_fine
+    
+#TODO
+#def fillMatrixWithJailAndActionCommunityCardsProb(matrix):
+#    TODO: Action Cards are on the fields 2,7,17,22,33
+#            1 Step: Get the probability to stay on the action field probStay
+#            2 Step: Calculate the probabilities to go to field x having 
+#            landed on an action field p_x_fromAction
+#            3 Step: For every state: multiply the probability to arrive at an 
+#            action field p_arrive by probStay and add p_arrive * p_x_fromAction 
+#            to all the fields x
+
+#    probStayCommunity = ...
+#     probStayAction = ...
+#     for probRow in matrix:
+#         for i in range(len(probRow):
+##             for the community cards
+#             if(i in [2,17] and not probRow[i] == 0):
+#            
+##             for the action cards
+#             elif(i in [7,22,33] and not probRow[i] == 0):
+                 
+             
+    
+def calculateStationaryVector(matrix):
+    """
+    -Calculates the stationnary vector of the matrix by 
+    solving the equation PI = PI * M for PI 
+    - Returns the stationnary vector PI
+    """
+    markovCh = markovChain(matrix)
+    markovCh.computePi('linear')
+    return markovCh.pi
 
 if __name__ == "__main__":
-    M_fine = fillMatrix()
-    print(len(M_fine[0]))
+    M_fine = fillMatrixWithDiceProb()
+    print(M_fine)
+    print(calculateStationaryVector(M_fine))
